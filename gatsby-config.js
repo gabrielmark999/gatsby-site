ee1a4999762f76fe5eac7d4e59ab0675a376f665
gabrielmark999 <gabrielmark999@gmail.com>
2022-01-13 16:57:07 -0300

    Finished Gatsby tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,16 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+        title: "My First Menu site",
     },
     plugins: [
         "gatsby-plugin-image",
         "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+              name: `blog`,
+              path: `${__dirname}/blog`,
+            }
+          },
     ]
 }
